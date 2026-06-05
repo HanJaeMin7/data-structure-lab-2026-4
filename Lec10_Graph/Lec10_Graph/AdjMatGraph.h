e1396f0f99ad33546440305fd7004153ec05da42
cotjdbs <csyoon1141@naver.com>
2026-06-05 10:06:27 +0900

    0605

--- Lec10_Graph/Lec10_Graph/AdjMatGraph.h
+++ Lec10_Graph/Lec10_Graph/AdjMatGraph.h
@@ -205,3 +205,79 @@ public:
         }
     }
 };
+
+class WGraphPrim : public WGraph {
+public:
+    int getMinVertex(bool* selected, int* dist) {
+        int minv = 0;
+        int mindist = INF;
+        for (int v=0; v<size; v++) 
+            if (!selected[v] && dist[v] < mindist) {
+                mindist = dist[v];
+                minv = v;
+            }
+        return minv;
+    }
+    void Prim(int s) {
+        bool selected[MAX_VTXS];
+        int dist[MAX_VTXS];
+        for (int i = 0;i < size;i++) {
+            dist[i] = INF;
+            selected[i] = false;
+        }
+        dist[s] = 0;
+        for (int i = 0;i < size;i++) {
+            int u = getMinVertex(selected, dist);
+            selected[u] = true;
+            if (dist[u] == INF)return;
+            printf("%c ", getVertex(u));
+            for (int v = 0; v < size;v++)
+                if (getEdge(u, v) != INF)
+                    if (!selected[v] && getEdge(u, v) < dist[v])
+                        dist[v] = getEdge(u, v);
+        }
+        printf("\n");
+    }
+    
+};
+
+// Dijkstra알고리즘의 최단 경로 탐색 기능이 추가된 그래프
+class WGraphDijkstra : public WGraph {
+    int dist[MAX_VTXS];          // 시작노드로부터의 최단경로 거리
+    bool found[MAX_VTXS];        // 방문한 정점 표시
+public:
+    int chooseVertex() {
+        int min = INF;
+        int minpos = -1;
+        for (int i = 0; i < size; i++)
+            if (dist[i] < min && !found[i]) {
+                min = dist[i];
+                minpos = i;
+            }
+        return minpos;
+    }
+    void printDistance() { // dist 상태를 출력하는 함수
+        for (int i = 0; i < size; i++)
+            printf("%5d", dist[i]);
+        printf("\n");
+    }
+    void ShortestPath(int start) {
+        for (int i = 0; i < size; i++) {
+            dist[i] = getEdge(start, i);
+            found[i] = false;
+        }
+        found[start] = true;                     // 시작노드 방문 표시
+        dist[start] = 0;                         // 최초 거리
+        for (int i = 0; i < size; i++) {
+            printf("Step%2d:", i + 1);
+            printDistance();
+            int u = chooseVertex();
+            found[u] = true;
+            for (int w = 0; w < size; w++) {
+                if (found[w] == false)
+                    if (dist[u] + getEdge(u, w) < dist[w])
+                        dist[w] = dist[u] + getEdge(u, w);
+            }
+        }
+    }
+};
--- Lec10_Graph/Lec10_Graph/main.cpp
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -2,40 +2,47 @@
 #include <stdio.h>
 int main() {
 	//AdjMatGraph g;
-	SrchAMGraph g;
+	//SrchAMGraph g;
 	
-	for (int i = 0;i < 4;i++)
-		g.insertVertex('A' + i);
-	g.insertEdge(0, 1);
-	g.insertEdge(0, 3);
-	g.insertEdge(1, 2);
-	g.insertEdge(1, 3);
-	g.insertEdge(2, 3);
-	printf("인접 행렬로 표현한 그래프\n");
-	g.display();
-	FILE* fp;
-	fopen_s(&fp, "../text_out.txt", "w");
-	g.display(fp);
-	fclose(fp);
+	//for (int i = 0;i < 4;i++)
+	//	g.insertVertex('A' + i);
+	//g.insertEdge(0, 1);
+	//g.insertEdge(0, 3);
+	//g.insertEdge(1, 2);
+	//g.insertEdge(1, 3);
+	//g.insertEdge(2, 3);
+	//printf("인접 행렬로 표현한 그래프\n");
+	//g.display();
+	//FILE* fp;
+	//fopen_s(&fp, "../text_out.txt", "w");
+	//g.display(fp);
+	//fclose(fp);
 
 	
-	printf("그래프(graph.txt)\n");
-	g.display();
+	//printf("그래프(graph.txt)\n");
+	//g.display();
 
-	printf("DFS ==> ");
-	g.resetVisited();
-	g.DFS(0);
+	//printf("DFS ==> ");
+	//g.resetVisited();
+	//g.DFS(0);
+	//	printf("\n");
+
+	//WGraph	g;
+	WGraphPrim g;
+	g.load("../graph.txt");
+	printf("MST By Kruskal's Algorithm\n");
+	g.Prim(0);
+	//g.display();
 	printf("\n");
 
-	WGraph	wg;
-	wg.load("../graph.txt");
-	wg.display();
-	printf("\n");
+	WGraphDijkstra wg;
+	wg.load("../graph_sp.txt");
+	printf("Shortest path by Dijkstra Algorithm\n");
+	wg.ShortestPath(0);
 	WGraphMST Mg;
 	Mg.load("../graph.txt");
-	// printf("입력 그래프: graph.txt\n");
-	// g.display("graph.txt");
-	printf("MST By Kruskal's Algorithm\n");
+	printf("입력 그래프: graph.txt\n");
+	//g.display("../graph.txt");
 	Mg.Kruskal();
 	return 0;
 }
